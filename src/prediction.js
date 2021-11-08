4598e3dac8c3aa3af0bc59616b04b736296b39a5
nonseodion <nonseifebhor@gmail.com>
2021-11-08 10:05:33 +0100

    adjusted time

--- src/prediction.js
+++ src/prediction.js
@@ -18,7 +18,7 @@ const getEpoch = predictionContract.methods.currentEpoch();
 
 //Schedule tasks to be run on the server.
 module.exports = () => {
-  cron.schedule(cronTime.everyMondayAt(9, 00), async function() {
+  cron.schedule(cronTime.everyMondayAt(9, 10), async function() {
     const epoch = await callTx(getEpoch);
 
     const callback = (status, ...msg) => {
@@ -32,7 +32,7 @@ module.exports = () => {
     await sendTx(startRound, callback);
   });
 
-  cron.schedule(cronTime.everyMondayAt(10, 1), async function() {
+  cron.schedule(cronTime.everyMondayAt(10, 11), async function() {
     const epoch = await callTx(getEpoch);
 
     const callback = (status, ...msg) => {
