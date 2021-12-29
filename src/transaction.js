d1a6c183726cb1dc8854411bac1b81ab9eb7bf84
nonseodion <nonseifebhor@gmail.com>
2021-12-29 17:13:25 +0100

    fixed gas price error, "transaction underpriced"

--- src/transaction.js
+++ src/transaction.js
@@ -1,17 +1,22 @@
-// const axios = require('axios');
+const axios = require('axios');
+const Web3 = require("web3");
 
-// async function getGasPrice() {
-//     let response = await axios.get('https://bscgas.info/gas?apikey=063f1d2dca3f471fb9bdaa1c4331f46e')
-//     return response.data.fast.toString();
-// }
+async function getGasPrice() {
+    let response = await axios.get('https://bscgas.info/gas?apikey=063f1d2dca3f471fb9bdaa1c4331f46e')
+    return response.data.fast.toString();
+}
 
 
 async function send(tx, callback){
   //passing true to callback indicates a successfull tx and vice-versa
   try {
     const gas = await tx.estimateGas();
-    // const gasPrice = Web3.utils.toWei(10, "gwei");
-    await tx.send({ gas: gas+100000,  })
+    const gasPrice = Web3.utils.toWei(
+      process.env.NODE_ENV === "development" ? "10" : await getGasPrice(),
+      "gwei"
+    );
+    console.log("gasPrice", gasPrice);
+    await tx.send({ gas: gas+100000, gasPrice  })
       .on('confirmation', function(confirmation, receipt){
         if(confirmation !== 0) return;
         callback(true, receipt.transactionHash);
