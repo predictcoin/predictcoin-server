74faacb136f864072e943a6a8154ac4afcb95ea9
nonseodion <nonseifebhor@gmail.com>
2022-05-04 00:04:53 +0100

    increased test Time to 10 minutes

--- src/data/football/variables.ts
+++ src/data/football/variables.ts
@@ -1,3 +1,3 @@
-export const playPeriod = 300; //used for testing and expressed in seconds
+export const playPeriod = 600; //used for testing and expressed in seconds
 export const dailyMatches = process.env.NODE_ENV === "development" ? 3 : 10;
 export const matchTime = process.env.NODE_ENV === "development" ? Math.round(0.5*playPeriod) : 105*60;
