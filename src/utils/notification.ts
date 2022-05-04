55f76505f01af52121a5942f34303c918e911de4
nonseodion <nonseifebhor@gmail.com>
2022-05-04 02:44:25 +0100

    moved stuff

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,4 +1,4 @@
 import Bugsnag from "@bugsnag/js";
-Bugsnag.start({ apiKey: 'c5733f7ac1fd1a57d80ced0183d40e49' });
+Bugsnag.start({ apiKey: process.env.BUGSNAG_API_KEY! });
 
 export default Bugsnag;
