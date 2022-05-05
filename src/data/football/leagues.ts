26276f7ef7ad2688b0f9a40c539bb57c5dffab6e
nonseodion <nonseifebhor@gmail.com>
2022-05-05 18:10:37 +0100

    added popular leagues to preffered Leagues

--- src/data/football/leagues.ts
+++ src/data/football/leagues.ts
@@ -1,11 +1,49 @@
 export const leagues = [
   1, // World cup
   2, // ChampionsLeague
+  531, // Uefa Super Cup
   39, // Premier League
+  3, // Europa
+  45, // FA Cup
+  4, // Euro Championship
+  5, // UEFA Nations League
+  9, // Copa America
+  6, // Africa Cup of Nations
+  19, // African Nations Championship
+  536, // CONCACAF Nations League
+
+  37, // World Cup - Qualification Intercontinental Play-offs
+  31, // World Cup - Qualification CONCACAF
+  32, // World Cup - Qualification Europe
+  33, // World Cup - Qualification Oceania
+  34, // World Cup - Qualification South America
+  29, // World Cup - Qualification Africa 
+  10, // Friendlies
+  667, // Friendlies Club
+
+  526, // Trophée des Champions
+  556, // Super Cup Spain
+  547, // Super Cup Italy
+  848, // UEFA Europa Conference League
   140, // La Liga
   135, // SerieA
   61, // Ligue 1
   78, // Bundesliga 1
+  15, // Fifa club World cup
+  137, // Coppa Italia
+
+
+  46, // EFL Trophy
+  40, // Championship
+  143, // Copa del Rey
+  81, // DFB Pokal
+  66, // Coupe de France
+  136, // Serie B
+  79, // Bundesliga 2
+  62, // Ligue 2
+  715, // DFB Junioren Pokal
+  138, // Serie C
+
   235, // Russia Premier League
   94, // Primeria League
   88, // Eredivisie
